324b51bf823e8558b4b625ee9000ff6864100518
sanskriti039 <sanskriti00039@gmail.com>
2025-07-21 11:12:28 +0100

    Commit the Task 1 and Task 2

---
+++ .venv/Task 1.py
@@ -0,0 +1,15 @@
+# Task 1 :  Perform Basic Mathematical Operations
+
+num1= int(input("Enter the first number: "))
+num2= int(input("Enter the second number: "))
+
+Addition= num1+num2
+Subtraction= num1-num2
+Multiplication= num1*num2
+Division= num1/num2
+
+print("Addition =", Addition)
+print("Subtraction =", Subtraction)
+print("Multiplication =", Multiplication)
+print("Division =", Division)
+
---
+++ .venv/Task 2.py
@@ -0,0 +1,9 @@
+# Task 2: Create a Personalized Greeting
+
+First_Name= input("Enter Your First Name: ")
+Last_Name= input("Enter Your Last Name: ")
+
+Full_Name= First_Name + " " + Last_Name
+
+print( "Hello, " +Full_Name+ "! " + "Welcome to Python Program.")
+
